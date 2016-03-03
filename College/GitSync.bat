172a9ed2fb41e206ba60f4a308d3ab7598c304ad
Michelfrancis Bustillos <michelfrancisb@gmail.com>
2016-03-03 16:57:47 -0500

    Update

--- College/GitSync.bat
+++ College/GitSync.bat
@@ -1,1 +1,6 @@
 robocopy "C:\Users\miche\Desktop\School\College\C++ Programming Language" "C:\Users\miche\Documents\GitHub\C_Programs\College" /MIR /z
+
+cd C:\Users\miche\Documents\GitHub\C_Programs
+C:\Users\miche\Documents\GitHub\git.lnk commit -am "Auto-committed on %date%"
+
+pause
