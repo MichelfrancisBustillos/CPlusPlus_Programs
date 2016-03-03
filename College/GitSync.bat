cc20490abcbffa7b7a015e1f882b79a28f57a504
Michelfrancis Bustillos <michelfrancisb@gmail.com>
2016-03-03 17:02:00 -0500

    Auto-committed on Thu 03/03/2016

--- College/GitSync.bat
+++ College/GitSync.bat
@@ -2,5 +2,6 @@ robocopy "C:\Users\miche\Desktop\School\College\C++ Programming Language" "C:\Us
 
 cd C:\Users\miche\Documents\GitHub\C_Programs
 call C:\Users\miche\AppData\Local\GitHub\PortableGit_cf76fc1621ac41ad4fe86c420ab5ff403f1808b9\cmd\git.exe commit -am "Auto-committed on %date%"
+call C:\Users\miche\AppData\Local\GitHub\PortableGit_cf76fc1621ac41ad4fe86c420ab5ff403f1808b9\cmd\git.exe push
 
 pause
